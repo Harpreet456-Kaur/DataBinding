93a583f568ace28dc62f1381348a70386ff083b9
Harpreet456-Kaur <125293917+Harpreet456-Kaur@users.noreply.github.com>
2023-02-28 16:00:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var value = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.intValue = 0
+        binding.activity = this
+    }
+
+    fun onPlusClick(){
+        value++
+        binding.intValue = value
+        binding.tvNum.text=binding.intValue.toString()
+    }
+    fun onMinusClick() {
+        value--
+        binding.intValue = value
+        binding.tvNum.text=binding.intValue.toString()
+    }
+    fun ResetClick() {
+        value = 0
+        binding.intValue=0
+        binding.tvNum.text=binding.intValue.toString()
+    }
+}
